3ace27ecdb2846fe1c8a94720607fa154c92420c
Piyush Khurana <Nanok4717@gmail.com>
2019-09-26 14:46:22 -0700

    Update W3Q6 elementAt.cpp

--- W3Q6 elementAt.cpp
+++ W3Q6 elementAt.cpp
@@ -1,4 +1,5 @@
-RationalNumber elementAt(int n) {
+// by recursion
+/*RationalNumber elementAt(int n) {
 	RationalNumber r1(2,6),r2(3,6);
 	if(n==1)
 		return r1;
@@ -6,6 +7,22 @@ RationalNumber elementAt(int n) {
 		return r2;
 	else {
 		return elementAt(n-1) + elementAt(n-2);
+	}
+}*/
+// By for loop
+RationalNumber elementAt(int n) {
+	RationalNumber r1(2,6),r2(3,6),r;
+	if(n==1)
+		return r1;
+	else if(n==2)
+		return r2;
+	else {
+		for(int i=3;i<=n;i++) {
+			r=r1+r2;
+			r1=r2;
+			r2=r;
+		}
+		return r;
 	}
 }
 //Test main()
